a5b57ea311bb2a4d96423a55c96d550be35fcaaf
Lars Vierbergen <lars.vierbergen@xenit.eu>
2022-12-07 16:04:09 +0100

    [ACC-449] Re-organize policy & webhook properties
    Split between configuration properties from spring-boot and variables used by policy & webhook actuators

--- contentgrid-spring-boot-actuators/src/main/java/com/contentgrid/spring/boot/actuator/policy/PolicyActuator.java
+++ contentgrid-spring-boot-actuators/src/main/java/com/contentgrid/spring/boot/actuator/policy/PolicyActuator.java
@@ -1,31 +1,33 @@
 package com.contentgrid.spring.boot.actuator.policy;
 
-import com.contentgrid.spring.boot.actuator.TemplateHelper;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.util.PropertyPlaceholderHelper;
+import org.springframework.util.SystemPropertyUtils;
 
 @WebEndpoint(id = "policy")
 @RequiredArgsConstructor
 public class PolicyActuator {
-    private final String path; // rego/policy.rego
-    private final TemplateHelper templateHelper;
+    private final Resource policyResource;
+    private final PolicyVariables policyVariables;
+    private static final PropertyPlaceholderHelper PROPERTY_PLACEHOLDER_HELPER = new PropertyPlaceholderHelper(
+            SystemPropertyUtils.PLACEHOLDER_PREFIX,
+            SystemPropertyUtils.PLACEHOLDER_SUFFIX
+    );
 
     @ReadOperation(producesFrom = RegoProducible.class)
     public String readPolicy() throws IOException {
-        Resource resource = new ClassPathResource(path);
-        if (resource.exists()) {
-            String contents = Files.readString(resource.getFile().toPath());
-            return this.templateHelper.templateSystemVars(contents);
+        if (policyResource.exists()) {
+            String contents = Files.readString(policyResource.getFile().toPath());
+
+            return PROPERTY_PLACEHOLDER_HELPER.replacePlaceholders(contents, policyVariables);
         } else {
-            throw new FileNotFoundException("rego file at " + path + " is not present");
+            throw new FileNotFoundException("rego file at " + policyResource.getDescription() + " is not present");
         }
     }
 }
--- contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/actuator/ActuatorAutoConfiguration.java
+++ contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/actuator/ActuatorAutoConfiguration.java
@@ -1,50 +1,57 @@
 package com.contentgrid.spring.boot.autoconfigure.actuator;
 
 import com.contentgrid.spring.boot.actuator.ContentGridApplicationProperties;
-import com.contentgrid.spring.boot.actuator.TemplateHelper;
 import com.contentgrid.spring.boot.actuator.policy.PolicyActuator;
+import com.contentgrid.spring.boot.actuator.policy.PolicyVariables;
 import com.contentgrid.spring.boot.actuator.webhooks.WebHooksConfigActuator;
+import com.contentgrid.spring.boot.actuator.webhooks.WebhookVariables;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.PropertyPlaceholderHelper;
-import org.springframework.util.SystemPropertyUtils;
 
 @Configuration
 @ConditionalOnClass({PolicyActuator.class, WebHooksConfigActuator.class})
 public class ActuatorAutoConfiguration {
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Bean
-    @ConditionalOnMissingBean(PolicyActuator.class)
-    PolicyActuator policyActuator(TemplateHelper templateHelper) {
-        return new PolicyActuator("rego/policy.rego", templateHelper);
+    PolicyActuator policyActuator(PolicyVariables policyVariables) {
+        if(policyVariables.getPolicyPackageName() == null) {
+            return null;
+        }
+        return new PolicyActuator(applicationContext.getResource("rego/policy.rego"), policyVariables);
     }
 
     @Bean
-    @ConditionalOnMissingBean(WebHooksConfigActuator.class)
-    WebHooksConfigActuator webHooksConfigActuator(TemplateHelper templateHelper) {
-        return new WebHooksConfigActuator("webhooks/config.json", templateHelper);
+    @ConditionalOnMissingBean(PolicyVariables.class)
+    PolicyVariables policyVariables(ContentGridApplicationProperties applicationProperties) {
+        return PolicyVariables.builder()
+                .policyPackageName(applicationProperties.getSystem().getPolicyPackage())
+                .build();
+    }
+
+    @Bean
+    WebHooksConfigActuator webHooksConfigActuator(WebhookVariables webhookVariables) {
+        return new WebHooksConfigActuator(applicationContext.getResource("webhooks/config.json"), webhookVariables);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(WebhookVariables.class)
+    WebhookVariables webhookVariables(ContentGridApplicationProperties applicationProperties) {
+        return WebhookVariables.builder()
+                .systemProperties(applicationProperties.getSystem())
+                .userVariables(applicationProperties.getVariables())
+                .build();
     }
 
     @Bean
     @ConfigurationProperties(prefix = "contentgrid")
     ContentGridApplicationProperties contentgridApplicationProperties() {
         return new ContentGridApplicationProperties();
-    }
-
-
-    @Bean
-    @ConditionalOnMissingBean(PropertyPlaceholderHelper.class)
-    PropertyPlaceholderHelper getPropertyPlaceHolderHelper() {
-        return new PropertyPlaceholderHelper(
-                SystemPropertyUtils.PLACEHOLDER_PREFIX,
-                SystemPropertyUtils.PLACEHOLDER_SUFFIX
-        );
-    }
-
-    @Bean
-    TemplateHelper templateHelper(PropertyPlaceholderHelper propertyPlaceholderHelper, ContentGridApplicationProperties props) {
-        return new TemplateHelper(propertyPlaceholderHelper, props);
     }
 }
