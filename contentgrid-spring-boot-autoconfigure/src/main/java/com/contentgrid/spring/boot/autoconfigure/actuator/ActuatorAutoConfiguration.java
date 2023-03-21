f779aca454e19e3a3711ebff15b463cd67dfce5b
Daniel Gradecak <daniel@pleosoft.com>
2023-03-21 07:39:37 +0100

    add ApplicationId and DeploymentId to Spring Boot info actuator

--- contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/actuator/ActuatorAutoConfiguration.java
+++ contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/actuator/ActuatorAutoConfiguration.java
@@ -1,19 +1,26 @@
 package com.contentgrid.spring.boot.autoconfigure.actuator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.info.BuildInfoContributor;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+import com.contentgrid.spring.boot.actuator.ContentGridApplicationInfoContributor;
 import com.contentgrid.spring.boot.actuator.ContentGridApplicationProperties;
 import com.contentgrid.spring.boot.actuator.policy.PolicyActuator;
 import com.contentgrid.spring.boot.actuator.policy.PolicyVariables;
 import com.contentgrid.spring.boot.actuator.webhooks.WebHooksConfigActuator;
 import com.contentgrid.spring.boot.actuator.webhooks.WebhookVariables;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnClass({PolicyActuator.class, WebHooksConfigActuator.class})
@@ -32,7 +39,6 @@ public class ActuatorAutoConfiguration {
 
     @Bean    
     @ConditionalOnBean(PolicyVariables.class)  
-    // TODO this is causing a problem, it should be in another autoconfigure file (@AutoconfigureAfter?)
     PolicyActuator policyActuator(PolicyVariables policyVariables) {
         return new PolicyActuator(applicationContext.getResource("classpath:rego/policy.rego"), policyVariables);
     }
@@ -55,5 +61,10 @@ public class ActuatorAutoConfiguration {
     @ConfigurationProperties(prefix = "contentgrid")
     ContentGridApplicationProperties contentgridApplicationProperties() {
         return new ContentGridApplicationProperties();
+    }
+
+    @Bean
+    InfoContributor buildInfoContributor(ContentGridApplicationProperties applicationProperties) {
+        return new ContentGridApplicationInfoContributor(applicationProperties.getSystem());
     }
 }
