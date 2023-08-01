f9cbc8d0b3876eead7519d65d7034f6ead926a32
Lars Vierbergen <lars.vierbergen@xenit.eu>
2023-08-01 08:14:17 +0200

    Add utility-methods to CurieProviderCustomizer to register a curie

--- contentgrid-spring-data-rest/src/main/java/com/contentgrid/spring/data/rest/hal/ContentGridSpringDataRestHalConfiguration.java
+++ contentgrid-spring-data-rest/src/main/java/com/contentgrid/spring/data/rest/hal/ContentGridSpringDataRestHalConfiguration.java
@@ -1,6 +1,5 @@
 package com.contentgrid.spring.data.rest.hal;
 
-import java.util.Map;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- contentgrid-spring-data-rest/src/main/java/com/contentgrid/spring/data/rest/hal/CurieProviderCustomizer.java
+++ contentgrid-spring-data-rest/src/main/java/com/contentgrid/spring/data/rest/hal/CurieProviderCustomizer.java
@@ -1,5 +1,15 @@
 package com.contentgrid.spring.data.rest.hal;
 
+import org.springframework.hateoas.UriTemplate;
+
 public interface CurieProviderCustomizer {
     CurieProviderBuilder customize(CurieProviderBuilder builder);
+
+    static CurieProviderCustomizer register(String curiePrefix, UriTemplate template) {
+        return builder -> builder.withCurie(curiePrefix, template);
+    }
+
+    static CurieProviderCustomizer register(String curiePrefix, String template) {
+        return register(curiePrefix, UriTemplate.of(template));
+    }
 }
