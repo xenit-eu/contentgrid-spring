f74ac873f98396345b47ec14688f1112b775ab17
Lars Vierbergen <lars.vierbergen@xenit.eu>
2023-08-01 10:22:09 +0200

    Add javadoc to CurieProvider{Builder,Customizer} interfaces

--- contentgrid-spring-data-rest/src/main/java/com/contentgrid/spring/data/rest/hal/CurieProviderBuilder.java
+++ contentgrid-spring-data-rest/src/main/java/com/contentgrid/spring/data/rest/hal/CurieProviderBuilder.java
@@ -3,7 +3,22 @@ package com.contentgrid.spring.data.rest.hal;
 import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.mediatype.hal.CurieProvider;
 
+/**
+ * Fluent builder for {@link CurieProvider}
+ */
 public interface CurieProviderBuilder {
+
+    /**
+     * Adds a mapping from CURIE prefix to a {@link UriTemplate} for resolving the CURIE against
+     * @param prefix CURIE prefix
+     * @param template Template to use to resolve the CURIE
+     * @return Copy with new curie mapping applied
+     */
     CurieProviderBuilder withCurie(String prefix, UriTemplate template);
+
+    /**
+     * Builds a {@link CurieProvider} with the mappings specified in the builder
+     * @return An immutable {@link CurieProvider} instance
+     */
     CurieProvider build();
 }
--- contentgrid-spring-data-rest/src/main/java/com/contentgrid/spring/data/rest/hal/CurieProviderCustomizer.java
+++ contentgrid-spring-data-rest/src/main/java/com/contentgrid/spring/data/rest/hal/CurieProviderCustomizer.java
@@ -2,13 +2,45 @@ package com.contentgrid.spring.data.rest.hal;
 
 import org.springframework.hateoas.UriTemplate;
 
+/**
+ * Customizer interface for {@link CurieProviderBuilder}.
+ * <p>
+ * By implementing this interface and exposing them as beans, they will automatically be applied to the default {@link org.springframework.hateoas.mediatype.hal.CurieProvider}
+ */
+@FunctionalInterface
 public interface CurieProviderCustomizer {
+
+    /**
+     * Customize the {@link CurieProviderBuilder}
+     * @param builder current builder
+     * @return builder with the desired customizations applied
+     */
     CurieProviderBuilder customize(CurieProviderBuilder builder);
 
+    /**
+     * Register a mapping between CURIE prefix and a URI template
+     *
+     * @param curiePrefix The CURIE prefix
+     * @param template The URI template to use
+     * @return customizer that registers a CURIE prefix mapping
+     *
+     * @see CurieProviderBuilder#withCurie(String, UriTemplate) for the builder method
+     * @see #register(String, String) for a shortcut that does not require a {@link UriTemplate} instance
+     */
     static CurieProviderCustomizer register(String curiePrefix, UriTemplate template) {
         return builder -> builder.withCurie(curiePrefix, template);
     }
 
+    /**
+     * Register a mapping between CURIE prefix and a URI template
+     *
+     * @param curiePrefix The CURIE prefix
+     * @param template The URI template to use
+     * @return customizer that registers a CURIE prefix mapping
+     *
+     * @see CurieProviderBuilder#withCurie(String, UriTemplate) for the builder method
+     * @see #register(String, UriTemplate) for passing a {@link UriTemplate} directly
+     */
     static CurieProviderCustomizer register(String curiePrefix, String template) {
         return register(curiePrefix, UriTemplate.of(template));
     }
