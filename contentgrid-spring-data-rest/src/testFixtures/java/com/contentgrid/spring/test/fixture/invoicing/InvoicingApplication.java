20ea8986fc9ff94b159e66aed9fa804dff2ccbe3
Lars Vierbergen <lars.vierbergen@xenit.eu>
2024-01-23 17:23:15 +0100

    [ACC-1159] Add a rabbitmq container to the invoicing test fixture
    The audit configuration requires a rabbitmq container to be available, unless it's explicitly disabled.
    But for a full-scale application test, it's a good idea to also integrate audit logging

--- contentgrid-spring-data-rest/src/testFixtures/java/com/contentgrid/spring/test/fixture/invoicing/InvoicingApplication.java
+++ contentgrid-spring-data-rest/src/testFixtures/java/com/contentgrid/spring/test/fixture/invoicing/InvoicingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
 
 @SpringBootApplication
 public class InvoicingApplication {
@@ -22,10 +23,17 @@ public class InvoicingApplication {
 
     @TestConfiguration(proxyBeanMethods = false)
     static class TestDatabaseConfiguration {
+
         @Bean
         @ServiceConnection
         PostgreSQLContainer postgreSQLContainer() {
             return new PostgreSQLContainer();
+        }
+
+        @Bean
+        @ServiceConnection
+        RabbitMQContainer rabbitMQContainer() {
+            return new RabbitMQContainer();
         }
     }
 
