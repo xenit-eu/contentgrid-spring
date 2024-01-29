9bcd14080dd5f0c1c25ad66df3c9fd21c59018e9
Lars Vierbergen <lars.vierbergen@xenit.eu>
2024-01-29 16:57:15 +0100

    [ACC-1159] Add operation kind to audit eventsµ

--- contentgrid-spring-audit-logging/src/main/java/com/contentgrid/spring/audit/handler/messaging/Jackson2AuditMessagingModule.java
+++ contentgrid-spring-audit-logging/src/main/java/com/contentgrid/spring/audit/handler/messaging/Jackson2AuditMessagingModule.java
@@ -1,17 +1,29 @@
 package com.contentgrid.spring.audit.handler.messaging;
 
 import com.contentgrid.spring.audit.event.AbstractAuditEvent;
+import com.contentgrid.spring.audit.event.AbstractEntityRelationAuditEvent;
+import com.contentgrid.spring.audit.event.EntityContentAuditEvent;
+import com.contentgrid.spring.audit.event.EntityItemAuditEvent;
+import com.contentgrid.spring.audit.event.EntityItemAuditEvent.Operation;
+import com.contentgrid.spring.audit.event.EntityRelationAuditEvent;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ClassSerializer;
+import com.fasterxml.jackson.databind.ser.std.EnumSerializer;
+import com.fasterxml.jackson.databind.ser.std.StdScalarSerializer;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
 
 public class Jackson2AuditMessagingModule extends SimpleModule {
 
@@ -22,7 +34,24 @@ public class Jackson2AuditMessagingModule extends SimpleModule {
         setMixInAnnotation(AbstractAuditEvent.class, AbstractAuditEventMixin.class);
     }
 
-    abstract static class AbstractAuditEventMixin {
+    @JsonPropertyOrder({
+            "operation",
+            "request.method",
+            "request.uri",
+            "response.status",
+            "response.location",
+            "subject.type",
+            "subject.id",
+            "subject.relation",
+            "subject.relation.id",
+            "subject.content",
+            "search"
+    })
+    private abstract static class AbstractAuditEventMixin {
+
+        @JsonProperty("operation")
+        @JsonSerialize(using = AuditEventOperationSerializer.class)
+        public abstract Object getOperation();
 
         @JsonProperty("request.method")
         public abstract String getRequestMethod();
@@ -61,16 +90,39 @@ public class Jackson2AuditMessagingModule extends SimpleModule {
         @JsonProperty("search")
         @JsonInclude(Include.NON_NULL)
         abstract Object getQueryParameters();
-
-        @JsonIgnore
-        public abstract Object getOperation();
     }
 
-    static class EntityTypeToNameSerializer extends ClassSerializer {
+    private static class EntityTypeToNameSerializer extends ClassSerializer {
 
         @Override
         public void serialize(Class<?> value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
             gen.writeString(value.getSimpleName());
+        }
+    }
+
+    private static class AuditEventOperationSerializer extends StdSerializer<Enum<?>> {
+
+        private static final Map<Enum<?>, String> TYPES = Map.of(
+                EntityItemAuditEvent.Operation.READ, "entity.read",
+                EntityItemAuditEvent.Operation.CREATE, "entity.create",
+                EntityItemAuditEvent.Operation.UPDATE, "entity.update",
+                EntityItemAuditEvent.Operation.DELETE, "entity.delete",
+                AbstractEntityRelationAuditEvent.Operation.READ, "relation.read",
+                AbstractEntityRelationAuditEvent.Operation.UPDATE, "relation.update",
+                AbstractEntityRelationAuditEvent.Operation.DELETE, "relation.delete",
+                EntityContentAuditEvent.Operation.READ, "content.read",
+                EntityContentAuditEvent.Operation.UPDATE, "content.update",
+                EntityContentAuditEvent.Operation.DELETE, "content.delete"
+        );
+
+        AuditEventOperationSerializer() {
+            super(Enum.class, true);
+        }
+
+        @Override
+        public void serialize(Enum<?> value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+            gen.writeString(Objects.requireNonNull(TYPES.get(value),
+                    () -> "Operation '%s#%s' is not mapped to a string".formatted(value.getClass(), value.name())));
         }
     }
 }
