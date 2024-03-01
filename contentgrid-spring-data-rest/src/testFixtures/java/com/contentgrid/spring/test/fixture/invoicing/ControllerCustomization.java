cb0b0b74b73f9804f4ae15182af8ef06d87736ac
niels cappelle <niels.cappelle@xenit.eu>
2024-03-01 14:31:10 +0100

    move AuditorAware inline as an anonymous class

--- contentgrid-spring-data-rest/src/testFixtures/java/com/contentgrid/spring/test/fixture/invoicing/ControllerCustomization.java
+++ contentgrid-spring-data-rest/src/testFixtures/java/com/contentgrid/spring/test/fixture/invoicing/ControllerCustomization.java
@@ -3,12 +3,17 @@ package com.contentgrid.spring.test.fixture.invoicing;
 import com.contentgrid.spring.test.fixture.invoicing.model.PromotionCampaign;
 import com.contentgrid.spring.test.fixture.invoicing.model.support.UserMetadata;
 import com.contentgrid.spring.test.fixture.invoicing.repository.PromotionCampaignRepository;
-import com.contentgrid.spring.test.fixture.invoicing.security.AuditorAwareImpl;
+import java.util.Optional;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
@@ -25,6 +30,24 @@ class ControllerCustomization implements RepositoryRestConfigurer {
 
     @Bean
     public AuditorAware<UserMetadata> auditorProvider() {
-        return new AuditorAwareImpl();
+        return new AuditorAware<UserMetadata>() {
+            @Override
+            @NotNull
+            public Optional<UserMetadata> getCurrentAuditor() {
+                try {
+                    return Optional.ofNullable(SecurityContextHolder.getContext())
+                            .map(SecurityContext::getAuthentication)
+                            .filter(Authentication::isAuthenticated)
+                            .map(Authentication::getPrincipal)
+                            .map(User.class::cast)
+                            .map(user -> new UserMetadata("12345", user.getUsername()));
+                } catch (ClassCastException exception) {
+                    // User.class::cast fails with @WithAnonymousUser
+                    // because the principal is then the String "anonymous".
+                    // Just return empty optional when the user is anonymous.
+                    return Optional.empty();
+                }
+            }
+        };
     }
 }
