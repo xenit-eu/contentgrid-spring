df12bb8aed7a8f78b37c10bf49e8d4b8caf13013
Toon Geens <toon.geens@xenit.eu>
2024-03-11 11:02:56 +0100

    Removed unnecesary read-only annotations

--- contentgrid-spring-data-support/src/main/java/com/contentgrid/spring/data/support/auditing/v1/UserMetadata.java
+++ contentgrid-spring-data-support/src/main/java/com/contentgrid/spring/data/support/auditing/v1/UserMetadata.java
@@ -1,8 +1,6 @@
 package com.contentgrid.spring.data.support.auditing.v1;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import com.fasterxml.jackson.annotation.JsonValue;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
@@ -25,6 +23,5 @@ public class UserMetadata {
     private String namespace;
 
     @JsonValue
-    @JsonProperty(access = Access.READ_ONLY)
     private String name;
 }
