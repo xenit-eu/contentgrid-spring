7e9cbdb318dcfb2c79f8ee2dfb7d56dc00d90f89
niels cappelle <niels.cappelle@xenit.eu>
2024-03-27 16:35:34 +0100

    add AnonymousTestAutoConfiguration to fix bootRun of local applications

---
+++ contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/security/AnonymousTestAutoConfiguration.java
@@ -0,0 +1,92 @@
+package com.contentgrid.spring.boot.autoconfigure.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.UUID;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+
+@AutoConfiguration(before = { OAuth2ResourceServerAutoConfiguration.class, SecurityAutoConfiguration.class })
+@ConditionalOnClass({SecurityFilterChain.class, HttpSecurity.class})
+@ConditionalOnProperty("contentgrid.spring.security.allow-anonymous")
+@EnableWebSecurity
+public class AnonymousTestAutoConfiguration {
+
+    @Bean
+    @ConditionalOnClass(BearerTokenAuthenticationToken.class)
+    public SecurityFilterChain anonymousJwtFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .anonymous(anonymous -> anonymous.authenticationFilter(new AnonymousJwtAuthenticationFilter()))
+                .build();
+    }
+
+    @Bean
+    @ConditionalOnMissingClass("org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken")
+    public SecurityFilterChain anonymousUsernamePasswordFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .anonymous(anonymous -> anonymous.authenticationFilter(
+                        new AnonymousUsernamePasswordAuthenticationFilter()))
+                .build();
+    }
+
+    private static String getKey() {
+        return UUID.randomUUID().toString();
+    }
+
+    static class AnonymousJwtAuthenticationFilter extends AnonymousAuthenticationFilter {
+
+        private String issuer = "http://localhost/realms/0";
+
+        public AnonymousJwtAuthenticationFilter() {
+            super(getKey(), "anonymous", AuthorityUtils.createAuthorityList("SCOPE_ANONYMOUS"));
+        }
+
+        @Override
+        protected Authentication createAuthentication(HttpServletRequest request) {
+            var username = getPrincipal().toString();
+            Jwt.Builder builder = Jwt.withTokenValue("token")
+                    .header("alg", "none")
+                    .subject(username)
+                    .issuer(this.issuer)
+                    .claim("scope", "anonymous")
+                    .claim("name", username);
+            Jwt jwt = builder.build();
+
+            return new JwtAuthenticationToken(jwt, getAuthorities());
+        }
+
+    }
+
+    static class AnonymousUsernamePasswordAuthenticationFilter extends AnonymousAuthenticationFilter {
+
+        public AnonymousUsernamePasswordAuthenticationFilter() {
+            super(getKey(), "anonymous", AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS"));
+        }
+
+        @Override
+        protected Authentication createAuthentication(HttpServletRequest request) {
+            var username = getPrincipal().toString();
+
+            return new UsernamePasswordAuthenticationToken(username, "password", getAuthorities());
+        }
+    }
+}
---
+++ contentgrid-spring-boot-autoconfigure/src/test/java/com/contentgrid/spring/boot/autoconfigure/security/AnonymousTestAutoConfigurationTest.java
@@ -0,0 +1,64 @@
+package com.contentgrid.spring.boot.autoconfigure.security;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.contentgrid.spring.boot.autoconfigure.security.AnonymousTestAutoConfiguration.AnonymousJwtAuthenticationFilter;
+import com.contentgrid.spring.boot.autoconfigure.security.AnonymousTestAutoConfiguration.AnonymousUsernamePasswordAuthenticationFilter;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.autoconfigure.logging.ConditionEvaluationReportLoggingListener;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.logging.LogLevel;
+import org.springframework.boot.test.context.FilteredClassLoader;
+import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken;
+import org.springframework.security.web.SecurityFilterChain;
+
+class AnonymousTestAutoConfigurationTest {
+
+    private final WebApplicationContextRunner contextRunner = new WebApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(AnonymousTestAutoConfiguration.class,
+                    OAuth2ResourceServerAutoConfiguration.class, SecurityAutoConfiguration.class
+            ));
+
+    @Test
+    void checkDefaults() {
+        contextRunner.withInitializer(ConditionEvaluationReportLoggingListener.forLogLevel(LogLevel.INFO))
+                .run(context -> {
+                    assertThat(context).hasNotFailed();
+                    assertThat(context).hasSingleBean(SecurityFilterChain.class);
+                    assertThat(context.getBean(SecurityFilterChain.class).getFilters()).noneMatch(filter ->
+                        filter instanceof AnonymousJwtAuthenticationFilter ||
+                                filter instanceof AnonymousUsernamePasswordAuthenticationFilter
+                    );
+                });
+    }
+
+    @Test
+    void checkWithProperty() {
+        contextRunner.withInitializer(ConditionEvaluationReportLoggingListener.forLogLevel(LogLevel.INFO))
+                .withPropertyValues("contentgrid.spring.security.allow-anonymous", "true")
+                .run(context -> {
+                    assertThat(context).hasNotFailed();
+                    assertThat(context).hasSingleBean(SecurityFilterChain.class);
+                    assertThat(context.getBean(SecurityFilterChain.class).getFilters()).anyMatch(filter ->
+                        filter instanceof AnonymousJwtAuthenticationFilter
+                    );
+                });
+    }
+
+    @Test
+    void checkWithoutOauth2ResourceServer() {
+        contextRunner.withInitializer(ConditionEvaluationReportLoggingListener.forLogLevel(LogLevel.INFO))
+                .withClassLoader(new FilteredClassLoader(BearerTokenAuthenticationToken.class))
+                .withPropertyValues("contentgrid.spring.security.allow-anonymous", "true")
+                .run(context -> {
+                    assertThat(context).hasNotFailed();
+                    assertThat(context).hasSingleBean(SecurityFilterChain.class);
+                    assertThat(context.getBean(SecurityFilterChain.class).getFilters()).anyMatch(filter ->
+                            filter instanceof AnonymousUsernamePasswordAuthenticationFilter
+                    );
+                });
+    }
+}
