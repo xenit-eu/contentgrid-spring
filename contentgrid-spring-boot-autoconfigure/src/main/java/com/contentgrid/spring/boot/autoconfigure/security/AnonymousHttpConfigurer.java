9c634a5c325a98ab7e71e7bb05659838e4870733
niels cappelle <niels.cappelle@xenit.eu>
2024-03-28 15:43:16 +0100

    remove AnonymousJwtAuthenticationFilter + fix anonymous users in JwtAuditorAware

--- contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/security/AnonymousHttpConfigurer.java
+++ contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/security/AnonymousHttpConfigurer.java
@@ -1,7 +1,6 @@
 package com.contentgrid.spring.boot.autoconfigure.security;
 
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.Locale;
 import java.util.UUID;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,8 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 
 public class AnonymousHttpConfigurer extends AbstractHttpConfigurer<AnonymousHttpConfigurer, HttpSecurity> {
@@ -24,66 +21,24 @@ public class AnonymousHttpConfigurer extends AbstractHttpConfigurer<AnonymousHtt
             http.csrf(AbstractHttpConfigurer::disable);
         }
         if (Boolean.TRUE.equals(allowUnauthenticated)) {
-            if (classExists(context, "org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationToken")) {
-                http.anonymous(anonymous -> anonymous.authenticationFilter(new AnonymousJwtAuthenticationFilter()));
-            } else {
-                http.anonymous(anonymous -> anonymous.authenticationFilter(
-                        new AnonymousUsernamePasswordAuthenticationFilter()));
-            }
+            http.anonymous(
+                    anonymous -> anonymous.authenticationFilter(new AnonymousUsernamePasswordAuthenticationFilter()));
         }
-    }
-
-    private boolean classExists(ApplicationContext context, String name) {
-        try {
-            var classLoader = context.getClassLoader();
-            return classLoader != null && classLoader.loadClass(name) != null;
-        } catch (ClassNotFoundException e) {
-            return false;
-        }
-    }
-
-    private static final String ANONYMOUS_NAME = "anonymous";
-    private static final String ANONYMOUS_AUTHORITY = "ANONYMOUS";
-    private static final String ANONYMOUS_ISSUER = "http://localhost/realms/0";
-    private static final String ANONYMOUS_PASSWORD = "password";
-
-    private static String getKey() {
-        return UUID.randomUUID().toString();
-    }
-
-    static class AnonymousJwtAuthenticationFilter extends AnonymousAuthenticationFilter {
-
-        public AnonymousJwtAuthenticationFilter() {
-            super(getKey(), ANONYMOUS_NAME, AuthorityUtils.createAuthorityList("SCOPE_" + ANONYMOUS_AUTHORITY));
-        }
-
-        @Override
-        protected Authentication createAuthentication(HttpServletRequest request) {
-            var username = getPrincipal().toString();
-            Jwt jwt = Jwt.withTokenValue("token")
-                    .header("alg", "none")
-                    .subject(username)
-                    .issuer(ANONYMOUS_ISSUER)
-                    .claim("scope", ANONYMOUS_AUTHORITY.toLowerCase(Locale.ROOT))
-                    .claim("name", username)
-                    .build();
-
-            return new JwtAuthenticationToken(jwt, getAuthorities());
-        }
-
     }
 
     static class AnonymousUsernamePasswordAuthenticationFilter extends AnonymousAuthenticationFilter {
 
         public AnonymousUsernamePasswordAuthenticationFilter() {
-            super(getKey(), ANONYMOUS_NAME, AuthorityUtils.createAuthorityList("ROLE_" + ANONYMOUS_AUTHORITY));
+            super(getKey(), "anonymous", AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS"));
         }
 
         @Override
         protected Authentication createAuthentication(HttpServletRequest request) {
-            var username = getPrincipal().toString();
+            return new UsernamePasswordAuthenticationToken(getPrincipal(), "password", getAuthorities());
+        }
 
-            return new UsernamePasswordAuthenticationToken(username, ANONYMOUS_PASSWORD, getAuthorities());
+        private static String getKey() {
+            return UUID.randomUUID().toString();
         }
     }
 }
