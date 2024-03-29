ef472c06ae2d709e87df09ec49f040ca77a320a2
niels cappelle <niels.cappelle@xenit.eu>
2024-03-29 14:06:21 +0100

    remove nested configuration from JpaAuditingAutoConfiguration

--- contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/data/audit/JpaAuditingAutoConfiguration.java
+++ contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/data/audit/JpaAuditingAutoConfiguration.java
@@ -1,20 +1,15 @@
 package com.contentgrid.spring.boot.autoconfigure.data.audit;
 
-import com.contentgrid.spring.boot.autoconfigure.data.audit.JpaAuditingAutoConfiguration.AuditorAwareConfiguration;
 import com.contentgrid.spring.data.support.auditing.v1.DefaultAuditorAware;
 import com.contentgrid.spring.data.support.auditing.v1.JwtAuditorAware;
 import com.contentgrid.spring.data.support.auditing.v1.UserMetadata;
 import jakarta.persistence.EntityManagerFactory;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -23,24 +18,18 @@ import org.springframework.security.oauth2.jwt.JwtClaimAccessor;
 @AutoConfiguration(after = {HibernateJpaAutoConfiguration.class})
 @ConditionalOnBean(EntityManagerFactory.class)
 @EnableJpaAuditing
-@Import(AuditorAwareConfiguration.class)
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class JpaAuditingAutoConfiguration {
+class JpaAuditingAutoConfiguration {
 
-    @Configuration
+    @Bean
+    @ConditionalOnClass({UserMetadata.class, SecurityContextHolder.class, JwtClaimAccessor.class})
+    AuditorAware<UserMetadata> jwtAuditorAware() {
+        return new JwtAuditorAware();
+    }
+
+    @Bean
     @ConditionalOnClass({UserMetadata.class, SecurityContextHolder.class})
-    static class AuditorAwareConfiguration {
-
-        @Bean
-        @ConditionalOnClass(JwtClaimAccessor.class)
-        AuditorAware<UserMetadata> jwtAuditorAware() {
-            return new JwtAuditorAware();
-        }
-
-        @Bean
-        @ConditionalOnMissingClass("org.springframework.security.oauth2.jwt.JwtClaimAccessor")
-        AuditorAware<UserMetadata> defaultAuditorAware() {
-            return new DefaultAuditorAware();
-        }
+    @ConditionalOnMissingClass("org.springframework.security.oauth2.jwt.JwtClaimAccessor")
+    AuditorAware<UserMetadata> defaultAuditorAware() {
+        return new DefaultAuditorAware();
     }
 }
