8a7d7c8276edcbb5d886fccbbe0efb6a52219b05
niels cappelle <niels.cappelle@xenit.eu>
2024-03-29 15:14:44 +0100

    add conditionals on missing beans to avoid duplicate bean exceptions if application defines own beans

--- contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/data/audit/JpaAuditingAutoConfiguration.java
+++ contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/data/audit/JpaAuditingAutoConfiguration.java
@@ -7,9 +7,10 @@ import jakarta.persistence.EntityManagerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.AuditingHandler;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,18 +18,20 @@ import org.springframework.security.oauth2.jwt.JwtClaimAccessor;
 
 @AutoConfiguration(after = {HibernateJpaAutoConfiguration.class})
 @ConditionalOnBean(EntityManagerFactory.class)
+@ConditionalOnMissingBean(AuditingHandler.class)
 @EnableJpaAuditing
 class JpaAuditingAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean(AuditorAware.class)
     @ConditionalOnClass({UserMetadata.class, SecurityContextHolder.class, JwtClaimAccessor.class})
     AuditorAware<UserMetadata> jwtAuditorAware() {
         return new JwtAuditorAware();
     }
 
     @Bean
+    @ConditionalOnMissingBean(AuditorAware.class)
     @ConditionalOnClass({UserMetadata.class, SecurityContextHolder.class})
-    @ConditionalOnMissingClass("org.springframework.security.oauth2.jwt.JwtClaimAccessor")
     AuditorAware<UserMetadata> defaultAuditorAware() {
         return new DefaultAuditorAware();
     }
