54aec7ab2c4ed70959772623500d98b8b110eefa
niels cappelle <niels.cappelle@xenit.eu>
2024-03-29 16:10:04 +0100

    simplify JwtAuditorAwareAutoConfiguration

--- contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/data/audit/JwtAuditorAwareAutoConfiguration.java
+++ contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/data/audit/JwtAuditorAwareAutoConfiguration.java
@@ -1,24 +1,19 @@
 package com.contentgrid.spring.boot.autoconfigure.data.audit;
 
-import com.contentgrid.spring.data.support.auditing.v1.DefaultAuditorAware;
 import com.contentgrid.spring.data.support.auditing.v1.JwtAuditorAware;
 import com.contentgrid.spring.data.support.auditing.v1.UserMetadata;
-import jakarta.persistence.EntityManagerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.JwtClaimAccessor;
 
-@AutoConfiguration(after = HibernateJpaAutoConfiguration.class, before = JpaAuditingAutoConfiguration.class)
-@ConditionalOnBean(EntityManagerFactory.class)
+@AutoConfiguration(before = JpaAuditingAutoConfiguration.class)
 @ConditionalOnMissingBean(AuditorAware.class)
 @ConditionalOnClass({UserMetadata.class, SecurityContextHolder.class, JwtClaimAccessor.class})
-public class JwtJpaAuditingAutoConfiguration {
+public class JwtAuditorAwareAutoConfiguration {
 
     @Bean
     AuditorAware<UserMetadata> jwtAuditorAware() {
