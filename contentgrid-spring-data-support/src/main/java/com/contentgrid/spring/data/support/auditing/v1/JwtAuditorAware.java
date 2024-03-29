0b16a9c83f3038be8321db713f19395131b4e2d0
niels cappelle <niels.cappelle@xenit.eu>
2024-03-29 16:43:41 +0100

    make JwtAuditorAware subclass of DefaultAuditorAware

--- contentgrid-spring-data-support/src/main/java/com/contentgrid/spring/data/support/auditing/v1/JwtAuditorAware.java
+++ contentgrid-spring-data-support/src/main/java/com/contentgrid/spring/data/support/auditing/v1/JwtAuditorAware.java
@@ -1,28 +1,25 @@
 package com.contentgrid.spring.data.support.auditing.v1;
 
 import java.util.Optional;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.JwtClaimAccessor;
 
-public class JwtAuditorAware implements AuditorAware<UserMetadata> {
+public class JwtAuditorAware extends DefaultAuditorAware {
 
     @Override
     public Optional<UserMetadata> getCurrentAuditor() {
-        return Optional.ofNullable(SecurityContextHolder.getContext())
+        var result = Optional.ofNullable(SecurityContextHolder.getContext())
                 .map(SecurityContext::getAuthentication)
                 .filter(Authentication::isAuthenticated)
-                .map(authentication -> {
-                    if (authentication.getPrincipal() instanceof JwtClaimAccessor jwt) {
-                        if (jwt.getSubject() != null && jwt.getIssuer() != null) {
-                            return new UserMetadata(jwt.getSubject(), jwt.getIssuer().toString(),
-                                    jwt.hasClaim("name") ? jwt.getClaimAsString("name") : jwt.getSubject());
-                        }
-                        return null;
-                    }
-                    return new UserMetadata(authentication.getName(), null, authentication.getName());
-                });
+                .map(Authentication::getPrincipal)
+                .filter(user -> user instanceof JwtClaimAccessor)
+                .map(JwtClaimAccessor.class::cast)
+                .filter(jwt -> jwt.getSubject() != null)
+                .filter(jwt -> jwt.getIssuer() != null)
+                .map(jwt -> new UserMetadata(jwt.getSubject(), jwt.getIssuer().toString(),
+                        jwt.hasClaim("name") ? jwt.getClaimAsString("name") : jwt.getSubject()));
+        return result.isEmpty() ? super.getCurrentAuditor() : result;
     }
 }
