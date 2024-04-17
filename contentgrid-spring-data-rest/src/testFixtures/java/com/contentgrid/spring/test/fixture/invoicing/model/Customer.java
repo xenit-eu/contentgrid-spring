21d8a421d88fa533ee2ed85cc63f228eeb6452db
niels cappelle <niels.cappelle@xenit.eu>
2024-04-17 16:50:33 +0200

    add validation groups to fix updating entity with removed required relation

--- contentgrid-spring-data-rest/src/testFixtures/java/com/contentgrid/spring/test/fixture/invoicing/model/Customer.java
+++ contentgrid-spring-data-rest/src/testFixtures/java/com/contentgrid/spring/test/fixture/invoicing/model/Customer.java
@@ -1,6 +1,7 @@
 package com.contentgrid.spring.test.fixture.invoicing.model;
 
 import com.contentgrid.spring.data.rest.validation.OnEntityDelete;
+import com.contentgrid.spring.data.rest.validation.OnEntityUpdate;
 import com.contentgrid.spring.data.support.auditing.v1.AuditMetadata;
 import com.contentgrid.spring.querydsl.annotation.CollectionFilterParam;
 import com.contentgrid.spring.querydsl.predicate.EntityId;
@@ -71,7 +72,7 @@ public class Customer {
 
     @Column(unique=true, nullable = false)
     @CollectionFilterParam(predicate = EqualsIgnoreCase.class)
-    @NotNull
+    @NotNull(groups = OnEntityUpdate.class)
     private String vat;
 
     @Embedded
