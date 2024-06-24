46ef296694b3435688d6370d5e40abe97dc433df
Roel <roel.schevenels@xenit.eu>
2024-06-24 11:36:48 +0200

    Prepare for flyway dependency structure update

--- contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/flyway/FlywayLegacyPostgresAutoConfiguration.java
+++ contentgrid-spring-boot-autoconfigure/src/main/java/com/contentgrid/spring/boot/autoconfigure/flyway/FlywayLegacyPostgresAutoConfiguration.java
@@ -1,7 +1,7 @@
 package com.contentgrid.spring.boot.autoconfigure.flyway;
 
 import org.flywaydb.core.Flyway;
-import org.flywaydb.database.postgresql.PostgreSQLConfigurationExtension;
+import org.flywaydb.core.internal.database.postgresql.PostgreSQLConfigurationExtension;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 @AutoConfiguration
 @AutoConfigureBefore(FlywayAutoConfiguration.class)
 @ConditionalOnClass({Flyway.class, PostgreSQLConfigurationExtension.class})
-public class FlywayPostgresAutoConfiguration {
+public class FlywayLegacyPostgresAutoConfiguration {
 
     /**
      * The Flyway default of transactional lock enabled conflicts with 'CREATE INDEX CONCURRENTLY' migrations, so it should be disabled by default.
@@ -20,7 +20,7 @@ public class FlywayPostgresAutoConfiguration {
      * @see <a href="https://documentation.red-gate.com/fd/postgresql-transactional-lock-184127530.html">Flyway documentation<a>
      */
     @Bean
-    FlywayConfigurationCustomizer postgresqlFlywayConfigurationCustomizerDisableTransactionalLock() {
+    FlywayConfigurationCustomizer postgresqlLegacyFlywayConfigurationCustomizerDisableTransactionalLock() {
         return configuration -> {
             configuration.getPluginRegister().getPlugin(PostgreSQLConfigurationExtension.class).setTransactionalLock(false);
         };
