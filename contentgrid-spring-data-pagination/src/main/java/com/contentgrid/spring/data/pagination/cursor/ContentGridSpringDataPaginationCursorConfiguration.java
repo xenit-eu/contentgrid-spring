4bb86d5e4f1ba0d99025121b0ed648fbcafd14a7
Lars Vierbergen <lars.vierbergen@xenit.eu>
2024-09-16 11:09:45 +0200

    Support changing configuration parameters on PageableCursorHandlerMethodArgumentResolver

--- contentgrid-spring-data-pagination/src/main/java/com/contentgrid/spring/data/pagination/cursor/ContentGridSpringDataPaginationCursorConfiguration.java
+++ contentgrid-spring-data-pagination/src/main/java/com/contentgrid/spring/data/pagination/cursor/ContentGridSpringDataPaginationCursorConfiguration.java
@@ -1,12 +1,14 @@
 package com.contentgrid.spring.data.pagination.cursor;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.data.web.HateoasPageableHandlerMethodArgumentResolver;
 import org.springframework.data.web.HateoasSortHandlerMethodArgumentResolver;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 
 @Configuration(proxyBeanMethods = false)
 public class ContentGridSpringDataPaginationCursorConfiguration {
@@ -19,16 +21,19 @@ public class ContentGridSpringDataPaginationCursorConfiguration {
     @Bean
     static BeanPostProcessor replaceHateoasPageableHandlerMethodArgumentResolverBeanPostProcessor(
             @Lazy HateoasSortHandlerMethodArgumentResolver sortHandlerMethodArgumentResolver,
-            @Lazy CursorCodec cursorCodec
+            @Lazy CursorCodec cursorCodec,
+            ObjectProvider<PageableHandlerMethodArgumentResolverCustomizer> resolverCustomizers
     ) {
         return new BeanPostProcessor() {
             @Override
             public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
                 if (bean instanceof HateoasPageableHandlerMethodArgumentResolver) {
-                    return new HateoasPageableCursorHandlerMethodArgumentResolver(
+                    var resolver = new HateoasPageableCursorHandlerMethodArgumentResolver(
                             sortHandlerMethodArgumentResolver,
                             cursorCodec
                     );
+                    resolverCustomizers.forEach(c -> c.customize(resolver));
+                    return resolver;
                 }
                 return bean;
             }
