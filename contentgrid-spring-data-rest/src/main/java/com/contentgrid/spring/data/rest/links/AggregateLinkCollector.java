5071c3065bfa1783fe7333161707786ba2207b08
Lars Vierbergen <lars.vierbergen@xenit.eu>
2025-01-13 12:54:19 +0100

    Make ContentGridLinkCollector generic
    Make it possible to scope a ContentGridLinkCollector to a single entity type with a generic type argument.
    The LambdaSafe class allows us to safely call only the collectors that match the entity type

--- contentgrid-spring-data-rest/src/main/java/com/contentgrid/spring/data/rest/links/AggregateLinkCollector.java
+++ contentgrid-spring-data-rest/src/main/java/com/contentgrid/spring/data/rest/links/AggregateLinkCollector.java
@@ -1,13 +1,15 @@
 package com.contentgrid.spring.data.rest.links;
 
+import java.util.Collection;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.util.LambdaSafe;
 import org.springframework.data.rest.webmvc.mapping.LinkCollector;
 import org.springframework.hateoas.Links;
 
 @RequiredArgsConstructor
 class AggregateLinkCollector implements LinkCollector {
     private final LinkCollector delegate;
-    private final Iterable<ContentGridLinkCollector> collectors;
+    private final Iterable<ContentGridLinkCollector<?>> collectors;
 
     @Override
     public Links getLinksFor(Object object) {
@@ -18,7 +20,10 @@ class AggregateLinkCollector implements LinkCollector {
     public Links getLinksFor(Object object, Links existing) {
         existing = delegate.getLinksFor(object, existing);
         for (var collector : collectors) {
-            existing = collector.getLinksFor(object, existing);
+            final Links finalExisting = existing;
+            existing = LambdaSafe.callback(ContentGridLinkCollector.class, collector, object, finalExisting)
+                    .invokeAnd(c -> c.getLinksFor(object, finalExisting))
+                    .get(finalExisting);
         }
         return existing;
     }
@@ -27,7 +32,10 @@ class AggregateLinkCollector implements LinkCollector {
     public Links getLinksForNested(Object object, Links existing) {
         existing = delegate.getLinksForNested(object, existing);
         for (var collector : collectors) {
-            existing = collector.getLinksForNested(object, existing);
+            final Links finalExisting = existing;
+            existing = LambdaSafe.callback(ContentGridLinkCollector.class, collector, object, finalExisting)
+                    .invokeAnd(c -> c.getLinksForNested(object, finalExisting))
+                    .get(finalExisting);
         }
         return existing;
     }
