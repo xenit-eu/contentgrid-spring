0e1fbcd95f3aca6b1b0e80db9cfd359beaea129e
niels cappelle <niels.cappelle@xenit.eu>
2025-02-07 17:06:26 +0100

    replace deprecated ContentStore

--- contentgrid-spring-data-rest/src/testFixtures/java/com/contentgrid/spring/test/fixture/invoicing/store/ShippingLabelContentStore.java
+++ contentgrid-spring-data-rest/src/testFixtures/java/com/contentgrid/spring/test/fixture/invoicing/store/ShippingLabelContentStore.java
@@ -1,7 +1,7 @@
 package com.contentgrid.spring.test.fixture.invoicing.store;
 
 import com.contentgrid.spring.test.fixture.invoicing.model.ShippingLabel;
-import org.springframework.content.commons.repository.ContentStore;
+import org.springframework.content.commons.store.ContentStore;
 import org.springframework.content.rest.StoreRestResource;
 
 @StoreRestResource(path = "shipping-labels")
